5ed2e4684a3756592d045b996edf2648d1364bbe
NPhenoMenon <nikhil.menon2002@gmail.com>
2023-02-20 16:40:36 +0000

    1. Added vs sdtm dataset 2. Fixed error with VISNUMEN data 3. Derived more variables

--- programs/adsl.R
+++ programs/adsl.R
@@ -21,6 +21,7 @@ ex = read_xpt("./sdtm/ds.xpt")
 sv = read_xpt("./sdtm/sv.xpt")
 mh = read_xpt("./sdtm/mh.xpt")
 sc = read_xpt("./sdtm/sc.xpt")
+vs = read_xpt("./sdtm/vs.xpt")
 qs = read_xpt("./sdtm/qs.xpt")
 
 #Loading supplementary data sets
@@ -68,6 +69,7 @@ ex <- convert_blanks_to_na(ex)
 sv <- convert_blanks_to_na(sv)
 mh <- convert_blanks_to_na(mh)
 sc <- convert_blanks_to_na(sc)
+vs <- convert_blanks_to_na(vs)
 qs <- convert_blanks_to_na(qs)
 
 # Extracting relevant dm variables
@@ -80,30 +82,18 @@ adsl <- dm %>%
 adsl <- adsl %>%
   mutate(TRT01P = ARM)
 
-# Deriving DSDECOD from ds
+# Deriving VISNUMEN from ds
+
+ds <- ds %>%
+  mutate(VISNUMEN = case_when(VISITNUM == 13 & DSTERM == "PROTOCOL COMPLETED" ~ 12,
+                              TRUE ~ VISITNUM))
 
 adsl <- adsl %>%
   derive_vars_merged(
     dataset_add = ds,
     filter_add = (DSCAT == "DISPOSITION EVENT"),
-    new_vars = vars(DSDECOD),
-    order = vars(DSDECOD),
-    mode = "first",
-    by_vars = vars(STUDYID, USUBJID)
-  )
-
-# Deriving VISNUMEN from ds
-
-ds <- ds %>%
-  mutate(VISNUMEN = case_when(VISITNUM == 13 & DSTERM == "PROTOCOL COMPLETED" ~ 12,
-                              DSTERM == "PROTOCOL COMPLETED" ~ VISITNUM))
-
-adsl <- adsl %>%
-  derive_vars_merged(
-    dataset_add = ds,
-    filter_add = (DSTERM == "PROTOCOL COMPLETED"),
-    new_vars = vars(VISNUMEN),
-    order = vars(VISNUMEN),
+    new_vars = vars(DSDECOD, VISNUMEN),
+    order = vars(DSDECOD, VISNUMEN),
     mode = "first",
     by_vars = vars(STUDYID, USUBJID)
   )
@@ -296,6 +286,119 @@ adsl <- adsl %>%
 adsl <- adsl %>%
   mutate(TRTSDT = format(as.Date(TRTSDT), "%d-%b-%Y"),
          VISIT1DT = format(as.Date(VISIT1DT), "%d-%b-%Y"))
+
+# Deriving DISONSDT from mh
+
+adsl <- adsl %>%
+  derive_vars_merged(
+    dataset_add = mh,
+    filter_add = (MHCAT == "PRIMARY DIAGNOSIS"),
+    new_vars = vars(DISONSDT = MHSTDTC),
+    order = vars(MHSTDTC),
+    mode = 'first',
+    by_vars = vars(STUDYID, USUBJID)
+  )
+
+adsl <- adsl %>%
+  mutate(DISONSDT = format(as.Date(DISONSDT), "%d-%b-%Y"))
+
+# Deriving EDUCLVL from sc
+
+adsl <- adsl %>%
+  derive_vars_merged(
+    dataset_add = sc,
+    filter_add = (SCTESTCD == "EDLEVEL"),
+    new_vars = vars(EDUCLVL = SCSTRESN),
+    order = vars(SCSTRESN),
+    mode = 'first',
+    by_vars = vars(STUDYID, USUBJID)
+  )
+
+# Deriving variables from vs
+
+adsl <- adsl %>%
+  derive_vars_merged(
+    dataset_add = vs,
+    filter_add = (VSTESTCD == "HEIGHT" & VISITNUM == 1),
+    new_vars = vars(HEIGHTBL = VSSTRESN),
+    order = vars(VSSTRESN),
+    mode = 'first',
+    by_vars = vars(STUDYID, USUBJID)
+  )
+
+adsl <- adsl %>%
+  derive_vars_merged(
+    dataset_add = vs,
+    filter_add = (VSTESTCD == "WEIGHT" & VISITNUM == 3),
+    new_vars = vars(WEIGHTBL = VSSTRESN),
+    order = vars(VSSTRESN),
+    mode = 'first',
+    by_vars = vars(STUDYID, USUBJID)
+  )
+
+# Deriving MSSETOT form qs
+
+qs <- qs %>%
+  select(USUBJID, QSORRES, QSCAT)
+
+qs <- qs %>%
+  mutate(QSORRES = as.integer(QSORRES))
+
+USUBJID = vector("character", 0)
+MMSETOT = vector("integer", 0)
+n_MMSETOT = 0
+
+for(i in 1:nrow(qs))
+{
+  if(i == 1)
+  {
+    subject = qs[1, 1]
+    USUBJID = c(subject)
+  }
+
+  if(qs[i, 3] == "MINI-MENTAL STATE")
+  {
+    n_MMSETOT = n_MMSETOT + qs[i, 2]
+  }
+
+  if(qs[i, 1] != subject)
+  {
+    MMSETOT = c(MMSETOT, n_MMSETOT)
+    subject = qs[i, 1]
+    USUBJID = c(USUBJID, subject)
+    n_MMSETOT = 0
+  }
+}
+
+MMSETOT = c(MMSETOT, n_MMSETOT)
+
+USUBJID <- paste(USUBJID)
+MMSETOT <- paste(MMSETOT)
+
+qs_adsl <- cbind(USUBJID, MMSETOT)
+
+adsl <- adsl %>%
+  derive_vars_merged(
+    dataset_add = as.data.frame(qs_adsl),
+    new_vars = vars(MMSETOT),
+    order = vars(MMSETOT),
+    mode = 'first',
+    by_vars = vars(USUBJID)
+  )
+
+#adsl <- adsl %>%
+#  mutate(AGEGR1N = case_when(AGE < 65 ~ 1,
+#                            AGE >= 65 & AGE <= 80 ~ 2,
+#                            AGE > 80 ~ 3),
+#         AGEGR1 = case_when(AGEGR1N == 1 ~ "<65",
+#                            AGEGR1N == 2 ~ "65-80",
+#                            AGEGR1N == 3 ~ ">80"),
+#         ARMN = case_when(ARM == "Placebo" ~ 0,
+#                          ARM == "Xanomeline Low Dose" ~ 1,
+#                          ARM == "Xanomeline High Dose" ~ 2),
+#         TRT01PN = ARMN,
+#         TRTDUR = TRTEDT - TRTSDT + 1,
+#         )
 
 adsl <- adsl %>%
   xportr_write("./adam/adsl2.xpt", label = "Subject-Level Analysis Dataset")
